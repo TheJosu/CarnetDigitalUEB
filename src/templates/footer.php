b8e1513046123de1c4380fa89eb475ea0ae315c6
TheJosu <minojosue66@gmail.com>
2024-09-04 17:18:10 -0500

    src

---
+++ src/templates/footer.php
@@ -0,0 +1,14 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto</title>
+    <link rel="stylesheet" href="../public/css/estilos.css">
+</head>
+<body>
+
+<!-- footer.php -->
+</body>
+</html>
