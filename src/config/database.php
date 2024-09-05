9759123f30c56984112ebbc23fb97a3e63b6a495
TheJosu <minojosue66@gmail.com>
2024-09-04 22:45:28 -0500

    cambio de bd

--- src/config/database.php
+++ src/config/database.php
@@ -1,14 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "AlumnosUEB";
+$host = getenv('MYSQL_HOST');
+$db = getenv('MYSQL_DB');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
