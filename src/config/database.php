ba3f78777418262593df2fae0eb0487c00b146d7
TheJosu <minojosue66@gmail.com>
2024-09-05 17:23:52 -0500

    cambios de datos

--- src/config/database.php
+++ src/config/database.php
@@ -1,13 +1,15 @@
 <?php
-$host = getenv('POSTGRES_HOST');
-$db = getenv('POSTGRES_DB');
-$user = getenv('POSTGRES_USER');
-$pass = getenv('POSTGRES_PASSWORD');
+$host = getenv('PGSQL_HOST') ?: 'dpg-crcip9rv2p9s73cgisig-a.oregon-postgres.render.com';  // Host que Render te proporcionó
+$db = getenv('PGSQL_DB') ?: 'alumnosueb';  // El nombre de la base de datos
+$user = getenv('PGSQL_USER') ?: 'root';  // El usuario de la base de datos
+$pass = getenv('PGSQL_PASSWORD') ?: 'YMoYLuvhnPNHv7ckBBsUQswAlG8an3Fq';  // La contraseña de la base de datos
 
 try {
-    $conn = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+    // Conectar usando la cadena de conexión a PostgreSQL
+    $conn = new PDO("pgsql:host=$host;port=5432;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    // Mostrar un mensaje de error si la conexión falla
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
