1de8839757a597eb08db3db50020068b943f6d2a
TheJosu <minojosue66@gmail.com>
2024-09-05 14:26:56 -0500

    cambio de bd

--- src/config/database.php
+++ src/config/database.php
@@ -1,11 +1,11 @@
 <?php
-$host = getenv('MYSQL_HOST');
-$db = getenv('MYSQL_DB');
-$user = getenv('MYSQL_USER');
-$pass = getenv('MYSQL_PASSWORD');
+$host = '102.177.161.34';
+$db = 'alumnosueb';
+$user = 'UEB';
+$pass = 'Cu3nta_BdUEB';
 
 try {
-    $conn = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
