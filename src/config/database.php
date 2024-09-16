7c6fa6d95fe48ad73b81cc4aa59084e0981315bc
TheJosu <minojosue66@gmail.com>
2024-09-16 09:49:31 -0500

    cambio de bd y sus conexiones

--- src/config/database.php
+++ src/config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('PGSQL_HOST');
-$dbname = getenv('PGSQL_DB');
-$username = getenv('PGSQL_USER');
-$password = getenv('PGSQL_PASSWORD');
+$host = getenv('PGSQL_HOST') ?: 'dpg-crk3m75ds78s73ei81gg-a.oregon-postgres.render.com';  // Host que Render te proporcionó
+$dbname = getenv('PGSQL_DB') ?: 'alumnosueb_zhn4';  // El nombre de la base de datos
+$username = getenv('PGSQL_USER') ?: 'root';  // El usuario de la base de datos
+$password = getenv('PGSQL_PASSWORD') ?: 'Tas5ts3uuW308ocoCuOvmzdBiZ7DGKiO';  // La contraseña de la base de datos
 
 try {
     $conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
