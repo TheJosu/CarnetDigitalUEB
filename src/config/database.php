5338448db33eb1805927a6306a28f118c1408687
TheJosu <minojosue66@gmail.com>
2024-09-15 19:59:33 -0500

    databd

--- src/config/database.php
+++ src/config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('PGSQL_HOST') ?: 'dpg-crcip9rv2p9s73cgisig-a.oregon-postgres.render.com';  // Host que Render te proporcionó
-$db = getenv('PGSQL_DB') ?: 'alumnosueb';  // El nombre de la base de datos
+$host = getenv('PGSQL_HOST') ?: 'dpg-crhhb8g8fa8c7392m6o0-a.oregon-postgres.render.com';  // Host que Render te proporcionó
+$db = getenv('PGSQL_DB') ?: 'prueba_bgqe';  // El nombre de la base de datos
 $user = getenv('PGSQL_USER') ?: 'root';  // El usuario de la base de datos
-$pass = getenv('PGSQL_PASSWORD') ?: 'YMoYLuvhnPNHv7ckBBsUQswAlG8an3Fq';  // La contraseña de la base de datos
+$pass = getenv('PGSQL_PASSWORD') ?: 'VN0adU5Mx2ob7bsPJl1YA6EXsshxat3P';  // La contraseña de la base de datos
 
 try {
     // Conectar usando la cadena de conexión a PostgreSQL
