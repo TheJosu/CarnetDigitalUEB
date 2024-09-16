ed3f5acb30c67abb9489efe5dddfbc94efb2a8d2
TheJosu <minojosue66@gmail.com>
2024-09-15 19:43:31 -0500

    cambio en config

--- src/config/database.php
+++ src/config/database.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "AlumnosUEB2";
+$host = getenv('PGSQL_HOST') ?: 'dpg-crcip9rv2p9s73cgisig-a.oregon-postgres.render.com';  // Host que Render te proporcionó
+$db = getenv('PGSQL_DB') ?: 'alumnosueb';  // El nombre de la base de datos
+$user = getenv('PGSQL_USER') ?: 'root';  // El usuario de la base de datos
+$pass = getenv('PGSQL_PASSWORD') ?: 'YMoYLuvhnPNHv7ckBBsUQswAlG8an3Fq';  // La contraseña de la base de datos
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+try {
+    // Conectar usando la cadena de conexión a PostgreSQL
+    $conn = new PDO("pgsql:host=$host;port=5432;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Mostrar un mensaje de error si la conexión falla
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
