576578362ce647f72e4cff0ef235b5efc2e514f3
TheJosu <minojosue66@gmail.com>
2024-09-15 20:16:52 -0500

    nuevo dabd

--- src/config/database.php
+++ src/config/database.php
@@ -2,7 +2,7 @@
 $host = getenv('PGSQL_HOST') ?: 'dpg-crhhb8g8fa8c7392m6o0-a.oregon-postgres.render.com';  // Host que Render te proporcionó
 $db = getenv('PGSQL_DB') ?: 'prueba_bgqe';  // El nombre de la base de datos
 $user = getenv('PGSQL_USER') ?: 'root';  // El usuario de la base de datos
-$pass = getenv('PGSQL_PASSWORD') ?: 'VN0adU5Mx2ob7bsPJl1YA6EXsshxat3P';  // La contraseña de la base de datos
+$pass = getenv('PGSQL_PASSWORD') ?: 'VN0adU5Mx2ob7bsPJl1YA6EXsshxat3P';  // La contraseña de la base de datos.
 
 try {
     // Conectar usando la cadena de conexión a PostgreSQL
