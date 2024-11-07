7b72d48b0fee98ea04f323aa863c30c2fd7d16ac
TheJosu <minojosue66@gmail.com>
2024-11-07 11:51:57 -0500

    cambio de bd cuentra principal

--- src/config/database.php
+++ src/config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('PGSQL_HOST') ?: 'dpg-cs48dnl2ng1s739fps90-a.oregon-postgres.render.com';  // Host que Render te proporcionó
-$dbname = getenv('PGSQL_DB') ?: 'alumnosueb_m41q';  // El nombre de la base de datos
+$host = getenv('PGSQL_HOST') ?: 'dpg-csmefnogph6c73ahavp0-a.oregon-postgres.render.com';  // Host que Render te proporcionó
+$dbname = getenv('PGSQL_DB') ?: 'alumnosueb_ix3r';  // El nombre de la base de datos
 $username = getenv('PGSQL_USER') ?: 'root';  // El usuario de la base de datos
-$password = getenv('PGSQL_PASSWORD') ?: 'Tdx2sdY4Vql3Gha15Kvseqn6W2inH9Wh';  // La contraseña de la base de datos
+$password = getenv('PGSQL_PASSWORD') ?: 'ApV2BeE3ZRm0RUMlaNKPuTFHmySQZLNE';  // La contraseña de la base de datos
 
 try {
     $conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
