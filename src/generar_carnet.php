979a1bec02867dc2368705f430cede6a2e92b687
TheJosu <minojosue66@gmail.com>
2024-11-15 17:11:23 -0500

    al inicio del tamaño nombre

--- src/generar_carnet.php
+++ src/generar_carnet.php
@@ -93,26 +93,18 @@ if (file_exists($fondoPath)) {
 }
 
 // Nombre
-$pdf->SetFont('Times', 'Bu', 90);
+$pdf->SetFont('Times', 'Bu', 50);
 $pdf->SetTextColor(0, 0, 0); // NEGRO
 // Obtener el ancho de la página
 $pdf->SetXY(round(90), round(1380));
 $pageWidth = $pdf->GetPageWidth();
-// Obtener el nombre del estudiante
-$nombreEstudiante = utf8_decode($student['nombre_estudiante']);
-// Ajustar el nombre si es muy largo
-$maxLongitudNombre = 50; // Máxima longitud del nombre
-if (strlen($nombreEstudiante) > $maxLongitudNombre) {
-    $nombreEstudiante = substr($nombreEstudiante, 0, $maxLongitudNombre) . "...";
-}
 // Obtener el ancho del texto del nombre del estudiante
-$nombreWidth = $pdf->GetStringWidth($nombreEstudiante);
+$nombreWidth = $pdf->GetStringWidth(utf8_decode($student['nombre_estudiante']));
 // Calcular la posición X para centrar el texto
 $nombreX = ($pageWidth - $nombreWidth) / 2;
-// Establecer la posición y el tamaño de la fuente para el nombre
-$pdf->SetFontSize(75);
+// Establecer la posición para el nombre centrado
 $pdf->SetXY(round($nombreX), $pdf->GetY() + 50); // Ajustar Y según necesites
-$pdf->Cell(round($nombreWidth), round(50), $nombreEstudiante, 0, 1, 'C');
+$pdf->Cell(round($nombreWidth), round(50), utf8_decode($student['nombre_estudiante']), 0, 1, 'C');
 
 // Cédula
 $pdf->SetFont('Times', 'I', 90);
