2096c5363817ce0ab4d9e68d644f06aae987f0fa
TheJosu <minojosue66@gmail.com>
2025-02-11 22:16:11 -0500

    bd

--- src/config/database.php
+++ src/config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('PGSQL_HOST') ?: 'dpg-ct8tq9m8ii6s73cge8m0-a.oregon-postgres.render.com';  // Host que Render te proporcionó
-$dbname = getenv('PGSQL_DB') ?: 'alumnosueb_ob2o';  // El nombre de la base de datos
+$host = getenv('PGSQL_HOST') ?: 'dpg-cum0dkt6l47c7392ta3g-a.oregon-postgres.render.com';  // Host que Render te proporcionó
+$dbname = getenv('PGSQL_DB') ?: 'alumnosueb_isvk';  // El nombre de la base de datos
 $username = getenv('PGSQL_USER') ?: 'root';  // El usuario de la base de datos
-$password = getenv('PGSQL_PASSWORD') ?: 'sPYk1tUFuXRcShPrSZ8Gd6dUp7F2DwC3';  // La contraseña de la base de datos
+$password = getenv('PGSQL_PASSWORD') ?: 'fx8YVGiALHpoM8hhEZSBTI2St6RCHCa4';  // La contraseña de la base de datos
 
 try {
     $conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
